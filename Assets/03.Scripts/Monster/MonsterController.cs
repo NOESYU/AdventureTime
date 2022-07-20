fd20292e9801fa1e4d4ff363b8224542a87af3e1
dprnd <ssunuu114@gmail.com>
2022-07-20 10:41:42 +0900

    Edit canvas prefabs to panel prefabs

--- Assets/03.Scripts/Monster/MonsterController.cs
+++ Assets/03.Scripts/Monster/MonsterController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterBar : MonoBehaviour
+public class MonsterController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
